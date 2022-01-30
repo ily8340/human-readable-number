b9f2a6392b77f6b12d5e3a386a2730342536aec4
ily8340 <96059753+ily8340@users.noreply.github.com>
2022-01-30 23:10:54 +0200

    fix: human-reable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,66 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number)
+ {
+        const numbers = [
+            '', 
+            'one', 
+            'two', 
+            'three', 
+            'four', 
+            'five',
+             'six', 
+             'seven',
+            'eight', 
+            'nine',
+            'ten', 
+            'eleven', 
+            'twelve', 
+            'thirteen', 
+            'fourteen', 
+            'fifteen',
+            'sixteen', 
+            'seventeen', 
+            'eighteen', 
+            'nineteen'
+        ];
+
+        const decimal = [
+        '', 
+        '', 
+        'twenty ', 
+        'thirty ', 
+        'forty ', 
+        'fifty ',
+        'sixty ', 
+        'seventy ', 
+        'eighty ', 
+        'ninety '
+        ];
+
+        const hundreds = [
+        '', 
+        'one hundred ', 
+        'two hundred ', 
+        'three hundred ', 
+        'four hundred ',
+        'five hundred ', 
+        'six hundred ', 
+        'seven hundred ', 
+        'eight hundred ', 
+        'nine hundred '
+        ];
+
+        if (number === 0) return 'zero';
+
+        const hundred = Math.floor(number / 100);
+        let result = hundreds[hundred];
+        let dec = number - hundred * 100;
+
+        if (dec < 20) {
+            result += numbers[dec];
+        } else {
+            dec = Math.floor(dec / 10);
+            const digit = number - hundred * 100 - dec * 10;
+            result += decimal[dec] + numbers[digit];
+        }
+        return result.trim();
+    }
